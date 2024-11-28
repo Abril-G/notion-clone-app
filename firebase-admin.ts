c9ea0bb43b231ed662afa5b57b38002810e5a587
BallinCat42 <abriel.devs@gmail.com>
2024-11-28 15:55:20 +0700

    again..

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,18 +1,18 @@
-import { initializeApp, getApp, getApps, App, cert } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
+// import { initializeApp, getApp, getApps, App, cert } from "firebase-admin/app";
+// import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("@/service_key.json");
+// const serviceKey = require("@/service_key.json");
 
-let app: App;
+// let app: App;
 
-if (getApps().length === 0) {
-    app = initializeApp({
-        credential: cert(serviceKey),
-    });
-} else {
-    app = getApp();
-}
+// if (getApps().length === 0) {
+//     app = initializeApp({
+//         credential: cert(serviceKey),
+//     });
+// } else {
+//     app = getApp();
+// }
 
-const adminDb = getFirestore(app);
+// const adminDb = getFirestore(app);
 
-export { app as adminApp, adminDb };
+// export { app as adminApp, adminDb };
